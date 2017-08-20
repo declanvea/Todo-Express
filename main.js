a54a95bd4e71b60f40bf4119c52db828ee773217
Declan Vea <declanpvea@gmail.com>
2017-08-20 08:38:28 -0400

    final

--- main.js
+++ main.js
@@ -2,71 +2,47 @@
  const express = require('express');
  const bodyParser = require('body-parser');
  const mustacheExpress = require('mustache-express');
- const app = express();
- const expressValidator = require('express-validator');
  const path = require('path');
+
+ const app = express();
+
+ app.use(express.static('public'));
+
  app.use(bodyParser.json());
- app.use(expressValidator());
  app.use(bodyParser.urlencoded({ extended: true }));
 
  app.engine('mustache', mustacheExpress());
  app.set('views', './views')
  app.set('view engine', 'mustache')
 
-const list = [
-    {todo: 'Wash the car', yetTodo: true},
+
+let list = {
+    data: [
+    {todo: 'Go to the grocery', yetTodo: true},
     {todo: 'Do laundry', yetTodo: false},
-];
+    {todo: 'Pack lunch', yetTodo: true},
+]
+};
 
-//setting the key and value for the render
-// const data = {
-//   todos: list
-// };
+let info = list.data;
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.render('todo', list);
 });
 
-app.post('/', function(req,res){
-  list.push({todo: req.body.inputfieldnameTBD, yetTodo: true});
+app.post('/', function(req,res) {
+  list.data.push({todo: req.body.task, yetTodo: true});
+  res.render('todo', list);
 });
-app.post('/formActionNameTBD', function(req, res){
-  console.log(req.body.buttonName);
-  let theOneWeWant = req.body.complete;
+
+app.post('/completed', function(req, res) {
   function findTodo(item){
-    return item.todo === theOneWeWant;}
-  console.log(list.find(findTodo));
-  list.find(findTodo).yetTodo = false;
+    if (item.todo === req.body.completed){
+        item.yetTodo = false;
+    }
+  }
+  info.find(findTodo);
   res.redirect('/');
 });
 
-
-
- // app.get("/", function(req, res){
- //   var html = `
- //     <h1> To Do List </h1>
- //     <form action="/" method="post">
- //     <input type="text" name="todo" placeholder="Add a todo...">
- //     <button type="submit">Submit</button>
- //     </form>
- //   `;
- //   res.send(html);
- // });
- // app.post("/", function(req, res){
- //   var todo = req.body.todo;
- //   var html = `
- //   <h1> To Do List </h1>
- //   <form action="/" method="post">
- //   <input type="text" name="todo" placeholder="Add a todo...">
- //   <button type="submit">Submit</button>
- //   </form>
- //   <input type="checkbox">
- //   ${todo}
- //   `;
- //   res.send(html);
- // });
-
-
- app.listen(3000, function(){
-   console.log('Another successful start? Somebody call this man "Butter" cause he is on a ROLL!!');
- });
+ app.listen(3000, () => console.log('Another successful start? Somebody call this man "Butter" cause he is on a ROLL!!'));
